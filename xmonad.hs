e375002db4a531b69debd36c1fbffe7604e1f1b5
otakuto <otakuto893@gmail.com>
2016-01-22 17:40:30 +0900

    add cursor disabler

--- xmonad.hs
+++ xmonad.hs
@@ -3,8 +3,47 @@ import XMonad.Util.Run
 import XMonad.Util.EZConfig
 import XMonad.Hooks.DynamicLog
 import XMonad.Layout.NoBorders
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Actions.KeyRemap
+import XMonad.Prompt
+import XMonad.Prompt.Shell
+import XMonad.Prompt.XMonad
+
+colorBlue = "#857da9"
+colorGreen = "#88b986"
+colorGray = "#676767"
+colorWhite = "#d3d7cf"
+colorGrayAlt = "#313131"
+colorNormalbg = "#1a1e1b"
+
+{-wsPP = xmobarPP
+	{
+	ppOrder = \(ws:l:t:_) -> [ws, t],
+	ppCurrent = xmobarColor colorGreen colorNormalbg,
+	ppUrgent = xmobarColor colorWhite colorNormalbg,
+	ppVisible = xmobarColor colorWhite colorNormalbg,
+	ppHidden = xmobarColor colorWhite colorNormalbg,
+	ppHiddenNoWindows = xmobarColor colorGray colorNormalbg,
+	ppTitle = xmobarColor colorWhite colorNormalbg,
+	ppWsSep = "",
+	ppSep = "  ::: "
+	-}
+
+--tm = do
+	{-
+	--setDefaultKeyRemap emptyKeyRemap [emap]
+	--setKeyRemap $ KeymapTable [((controlMask, xK_h), (0, xK_Left))]
+	--setDefaultKeyRemap emptyKeyRemap [emptyKeyRemap, dvorakProgrammerKeyRemap]
+	--setKeyRemap $  emptyKeyRemap ++ KeymapTable [((controlMask, xK_h), (0, xK_Left))]
+	--KeymapTable [((controlMask, xK_h), (0, xK_Left))]
+	--let keys = KeymapTable [((0, xK_a), (0, xK_t))]
+	-- ++ buildKeyRemapBindings [emptyKeyRemap]
+	--setKeyRemap $ KeymapTable [((0, xK_a), (shiftMask, xK_t))]
+	setKeyRemap $ (KeymapTable [((shiftMask, xK_t), (0, xK_a))])
+	-}
 
 main = do
+--bar <- spawnPipe "xmobar"
 xmonad $ defaultConfig
 	{
 	terminal = "urxvtc",
@@ -13,13 +52,20 @@ xmonad $ defaultConfig
 	normalBorderColor = "#808080",
 	modMask = mod1Mask,
 	layoutHook = smartBorders Full ||| Tall 1 (1/100) (1/2)
+	--startupHook = tm
+	--logHook = dynamicLogWithPP $ wsPP { ppOutput = hPutStrLn bar}
 	}
 
 	`additionalKeysP`
 	[
+    ("M-q", spawn "xmonad --recompile; xmonad --restart"),
 	("M-w", spawn "firefox"),
 	("M-<F4>", kill),
 	("<Print>", spawn "scrot /$HOME/work/screenshot/%Y%m%d-%H%M%S.png"),
-	("C-<Print>", spawn "scrot /$HOME/work/screenshot/%Y%m%d-%H%M%S.png -u")
+	("C-<Print>", spawn "scrot /$HOME/work/screenshot/%Y%m%d-%H%M%S.png -u"),
+	("M-<F9>", spawn "xinput --disable Mouse0"),
+	("M-S-<F9>", spawn "xinput --enable Mouse0")
+	--("M-x", xmonadPrompt defaultXPConfig),
+	--("M-b", prompt "urxvt" greenXPConfig)
 	]
 
